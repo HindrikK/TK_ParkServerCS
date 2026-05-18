c78915855649bcceb40de5497dfbde099cac8a31
Hindrik <hindrik.kilter@gmail.com>
2026-05-18 21:37:54 +0300

    Updated framework version from 4 to 4.8

--- Eleon_SCADA/Properties/AssemblyInfo.cs
+++ Eleon_SCADA/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Eleon SCADA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eleon")]
+[assembly: AssemblyCompany("Troonik OÜ")]
 [assembly: AssemblyProduct("Eleon SCADA")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.8")]
-[assembly: AssemblyFileVersion("1.0.4.8")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
